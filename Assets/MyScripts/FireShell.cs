a42c9fd346f503bd6b54891c0bf1162b525655d4
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2023-04-16 13:20:39 +0300

    add

---
+++ Assets/MyScripts/FireShell.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireShell : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+
+    [SerializeField]
+    GameObject turret;
+
+    [SerializeField]
+    GameObject enemy;
+
+    [SerializeField]
+    private float rotSpeed = 2f;
+
+    // Start is called before the first frame update
+    void CreateBullet()
+    {
+        Instantiate(bullet, turret.transform.position, turret.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = enemy.transform.position - this.transform.position.normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,lookRotation,Time.deltaTime * rotSpeed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateBullet();
+        }
+    }
+}
