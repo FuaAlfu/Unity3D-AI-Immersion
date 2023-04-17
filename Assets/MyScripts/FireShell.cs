6fa614b076e1b52f747710dac8d8612630d7bd74
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2023-04-17 15:27:38 +0300

    add

--- Assets/MyScripts/FireShell.cs
+++ Assets/MyScripts/FireShell.cs
@@ -11,7 +11,13 @@ public class FireShell : MonoBehaviour
     GameObject turret;
 
     [SerializeField]
+    Transform turretBase;
+
+    [SerializeField]
     GameObject enemy;
+
+    [SerializeField]
+    float speed = 15f;
 
     [SerializeField]
     private float rotSpeed = 2f;
@@ -22,12 +28,47 @@ public class FireShell : MonoBehaviour
         Instantiate(bullet, turret.transform.position, turret.transform.rotation);
     }
 
+    void RotateTurret()
+    {
+        float? angle = CaclulateAngle(true);
+        if(angle != null)
+        {
+            turretBase.localEulerAngles = new Vector3(360f - (float)angle, 0f, 0f);
+        }
+    }
+
+    float? CaclulateAngle(bool low)
+    {
+        Vector3 targetDir = enemy.transform.position - this.transform.position;
+        float y = targetDir.y;
+        targetDir.y = 0f;
+        float x = targetDir.magnitude;
+        float gravity = 9.8f;
+        float sSqr = speed * speed;
+        float underTheSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
+
+        if (underTheSqrRoot >= 0f)
+        {
+            float root = Mathf.Sqrt(underTheSqrRoot);
+            float highAngle = sSqr + root;
+            float lowAngle = sSqr - root;
+
+            if (low)
+                return (Mathf.Atan2(lowAngle, gravity * x) * Mathf.Rad2Deg);
+            else
+                return (Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
+        }
+        else
+            return null;
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector3 direction = enemy.transform.position - this.transform.position.normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation,lookRotation,Time.deltaTime * rotSpeed);
+        RotateTurret();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             CreateBullet();
